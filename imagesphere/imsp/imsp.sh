70153c5415e5a5358f64ad07cfbcdf9ea1a960f8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-28 14:05:41 -0500

    Pipe command output to null

--- imagesphere/imsp/imsp.sh
+++ imagesphere/imsp/imsp.sh
@@ -1,6 +1,6 @@
 cd /tmp/imsp
-ffmpeg -i input/* -frames:v 1 -y input/input.png
-xvfb-run /tmp/imsp/blender/blender -b anysphere.blend -P sphere.py -o ./out/ input/input.png
+ffmpeg -i input/* -frames:v 1 -y input/input.png > /dev/null 2>&1
+xvfb-run /tmp/imsp/blender/blender -b anysphere.blend -P sphere.py -o ./out/ input/input.png > /dev/null 2>&1
 cd out
-gifski *.png --fps 30 -o out.gif
+gifski *.png --fps 30 -o out.gif > /dev/null 2>&1
 rm *.png
