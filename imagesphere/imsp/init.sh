e749c2c42fa610c83241cf0af0c2095a8419ddd9
BuildTools <unconfigured@null.spigotmc.org>
2021-02-28 11:49:36 -0500

    Update blender version

--- imagesphere/imsp/init.sh
+++ imagesphere/imsp/init.sh
@@ -1,7 +1,7 @@
 mkdir -p /tmp/imsp/{input,out}
 cp ./* /tmp/imsp
 cd /tmp/imsp
-wget https://builder.blender.org/download/blender-2.93.0-f7933d0744df-linux64.tar.xz
-tar -xf blender-2.93.0-f7933d0744df-linux64.tar.xz
-mv blender-2.93.0-f7933d0744df-linux64 blender
-rm blender-2.93.0-f7933d0744df-linux64.tar.xz
+wget https://builder.blender.org/download/blender-2.93.0-f3d60c68ef46-linux64.tar.xz
+tar -xf blender-2.93.0-f3d60c68ef46-linux64.tar.xz
+mv blender-2.93.0-f3d60c68ef46-linux64 blender
+rm blender-2.93.0-f3d60c68ef46-linux64.tar.xz
