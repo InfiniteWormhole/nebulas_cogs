3bbfa4a3f91f69215e4c42329bf7bc103bab2bb6
BuildTools <unconfigured@null.spigotmc.org>
2021-03-01 12:51:50 -0500

    Change blender version (again...)

--- imagesphere/imsp/init.sh
+++ imagesphere/imsp/init.sh
@@ -1,7 +1,7 @@
 mkdir -p /opt/imsp/{input,out}
 cp ./* /opt/imsp
 cd /opt/imsp
-wget https://builder.blender.org/download/blender-2.93.0-f3d60c68ef46-linux64.tar.xz
-tar -xf blender-2.93.0-f3d60c68ef46-linux64.tar.xz
-mv blender-2.93.0-f3d60c68ef46-linux64 blender
-rm blender-2.93.0-f3d60c68ef46-linux64.tar.xz
+wget https://www.blender.org/download/Blender2.92/blender-2.92.0-linux64.tar.xz/
+tar -xf blender-2.92.0-linux64.tar.xz
+mv blender-2.92.0-linux64 blender
+rm blender-2.92.0-linux64.tar.xz
