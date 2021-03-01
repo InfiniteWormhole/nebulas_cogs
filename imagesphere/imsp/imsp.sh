0a3362518bab498bb47e3f7a8654749d4696c45e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-01 12:59:32 -0500

    Change tmp to opt in imsp.sh

--- imagesphere/imsp/imsp.sh
+++ imagesphere/imsp/imsp.sh
@@ -1,6 +1,6 @@
 cd /opt/imsp
 ffmpeg -i input/* -frames:v 1 -y input/input.png > /dev/null 2>&1
-xvfb-run /tmp/imsp/blender/blender -b anysphere.blend -P sphere.py -o ./out/ input/input.png > /dev/null 2>&1
+xvfb-run /opt/imsp/blender/blender -b anysphere.blend -P sphere.py -o ./out/ input/input.png > /dev/null 2>&1
 cd out
 gifski *.png --fps 30 -o out.gif > /dev/null 2>&1
 rm *.png
