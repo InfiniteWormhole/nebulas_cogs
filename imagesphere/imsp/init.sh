611f993dabd62d4de21b2da02290dfac03dc04c3
BuildTools <unconfigured@null.spigotmc.org>
2021-03-01 12:53:56 -0500

    Switch blender mirrors.....

--- imagesphere/imsp/init.sh
+++ imagesphere/imsp/init.sh
@@ -1,7 +1,7 @@
 mkdir -p /opt/imsp/{input,out}
 cp ./* /opt/imsp
 cd /opt/imsp
-wget https://www.blender.org/download/Blender2.92/blender-2.92.0-linux64.tar.xz/
+wget https://mirror.clarkson.edu/blender/release/Blender2.92/blender-2.92.0-linux64.tar.xz
 tar -xf blender-2.92.0-linux64.tar.xz
 mv blender-2.92.0-linux64 blender
 rm blender-2.92.0-linux64.tar.xz
